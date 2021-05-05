cf2ad923876c6171214dca107f2dc37e4c5fe281
Ilya Baran <baran37@gmail.com>
2021-05-05 16:33:57 -0400

    Export AbstractBlock

--- Julia/ModelBlocks/src/ModelBlocks.jl
+++ Julia/ModelBlocks/src/ModelBlocks.jl
@@ -7,7 +7,7 @@ include("variables.jl")
 export AbstractReaction, SimpleReaction, GeneralRateReaction, GeneralReaction
 include("reactions.jl")
 
-export Block, getVariables, getParameters, runBlock, BlockWithBindings, BlockWithOutputs, getOutputs
+export AbstractBlock, Block, getVariables, getParameters, runBlock, BlockWithBindings, BlockWithOutputs, getOutputs
 export solutionToMatlab, solutionToMatrix, solutionToVariables
 include("block.jl")
 
